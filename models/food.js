e7e21b5bdf881411dec61d22d02fde72c3e94489
T3tje <tilman2013@gmail.com>
2022-03-22 22:23:51 +0100

    neu

--- models/food.js
+++ models/food.js
@@ -8,9 +8,11 @@ const foodSchema = new mongoose.Schema({
    fett: {type: Number, required: true},
    zucker: {type: Number, required: true},
    rate: {type: String, required: true},
-   likes: {type: Number, required: true},
+   likes: {type: Number, required: false},
    kcal: {type: Number, required: true},
-   barcode: {type: Number, required: false}
+   barcode: {type: Number, required: false},
+   picLink: {type: String, required: false},
+   zusatzstoffe: {type: Boolean, required: false }
 })
 
 foodSchema.set("toJSON", {

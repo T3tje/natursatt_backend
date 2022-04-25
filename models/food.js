6d6d6c863cc43fda9b9644e28b098516f94ae8e4
T3tje <tilman2013@gmail.com>
2022-04-25 18:43:07 +0200

    first part adding lebensmittel

--- models/food.js
+++ models/food.js
@@ -4,15 +4,16 @@ const foodSchema = new mongoose.Schema({
    name: {type: String, required: true, unique:true },
    kh: {type: Number, required: true},
    ew: {type: Number, required: true},
-   ballast: {type: Number, required: true},
    fett: {type: Number, required: true},
+   ballast: {type: Number, required: true},
    zucker: {type: Number, required: true},
    rate: {type: String, required: true},
    likes: {type: Number, required: false},
    kcal: {type: Number, required: true},
    barcode: {type: Number, required: false},
    picLink: {type: String, required: false},
-   zusatzstoffe: {type: Boolean, required: false }
+   zusatzstoffe: {type: Boolean, required: false },
+   affiLink: {type: String, required: false}
 })
 
 foodSchema.set("toJSON", {

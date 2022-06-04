69ae55d2b7dda9fbce8ce0512475067910b82166
T3tje <tilman2013@gmail.com>
2022-06-04 14:42:34 +0200

    saveNewPassword

---
+++ controller/passwordResetCheckRouter.js
@@ -0,0 +1,21 @@
+const passwordResetCheckRouter = require("express").Router()
+const User = require("../models/user")
+const jwt = require("jsonwebtoken")
+
+passwordResetCheckRouter.post("/", async (request, response) => {
+   const {userId, token} = request.body
+
+   const user = await User.findById( userId )
+
+   const secret = process.env.SECRET + user.passwordHash
+
+   const decodedToken = await jwt.verify(token, secret)
+
+   console.log("decodedToken", decodedToken)
+   
+   response.status(200).send()
+
+})
+
+module.exports = passwordResetCheckRouter
+
--- controller/passwordRouter.js
+++ controller/passwordRouter.js
@@ -22,7 +22,7 @@ passwordRouter.post("/", async (request, response) => {
       
       const token = jwt.sign(userDataForToken, secret, { expiresIn: 1000 * 60 * 5 })
      
-      const emailLink = `${config.OWN_URL}/authentication/newpassword?token=${token}&id=${user.id}`
+      const emailLink = `${config.OWN_URL}/authentication/newpassword?frame=${token}?frame=${user.id}`
       /*  const subject = "Passwort wiederherstellen"
       const emailContent = `Bitte klicke auf den folgenden Link um Dein neues Passwort festzulegen: ${emailLink}`
       const htmlContent = "<h1>Test123</h1><p>Ptest123</p>"

98e98864d3de964551c5a23972c024ad36bd4a6d
T3tj3 <tilman2013@gmail.com>
2022-12-02 16:18:12 +0100

    trackedEventSchema

--- controller/startRouter.js
+++ controller/startRouter.js
@@ -1,16 +1,13 @@
 const startRouter = require("express").Router()
 
 startRouter.get("/", (request, response) => {
-
-   console.log("ip-adresse vom user", request)
    if (request.isAuthenticated()) {
-      
       const userObject = {
-         name:request.user.name,
+         name: request.user.name,
          id: request.user.id,
-         isAdmin: request.user.isAdmin
+         isAdmin: request.user.isAdmin,
       }
-      
+
       response.status(200).send(userObject)
    } else {
       response.send(null)
---
+++ models/trackedevents.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose")
+
+const trackedEventSchema = new mongoose.Schema({
+   art: {type: String, required: true, unique: true},
+   date: {type: Date},
+   ip: {type: String, required: false}
+})
+
+trackedEventSchema.set("toJSON", {
+   transform: (document, returnedObject) => {
+      returnedObject.id = returnedObject._id.toString()
+      delete returnedObject.__v
+   }
+})
+
+const TrackedEvent = mongoose.model("TrackedEvent", trackedEventSchema)
+
+module.exports = TrackedEvent
